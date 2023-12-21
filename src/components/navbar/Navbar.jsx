dc6fb32a2e4c23299301b782b385dbf6d951f3c0
Aryan Tele <58328909+AryanTele@users.noreply.github.com>
2023-12-22 00:27:18 +0530

    Designing Cursor and Contact Pagre

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,3 +1,4 @@
+import Sidebar from "../sidebar/Sidebar";
 import "./navbar.scss";
 import { motion } from "framer-motion";
 
@@ -5,11 +6,13 @@ const Navbar = () => {
   return (
     <div className="navbar">
       {/* {Sidebar} */}
+      <Sidebar />
+
       <div className="wrapper">
-        //framer animation
+        {/* framer animation */}
         <motion.span
           initial={{ opacity: 0, scale: 0.5 }}
-          animate={{ opacity: 1, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
           transition={{ duration: 0.5 }}
         >
           Aryan Tele
