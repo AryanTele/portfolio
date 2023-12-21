7288cbfd4a4fbc3176f467d2b98bc00a3ed60fc6
Aryan Tele <58328909+AryanTele@users.noreply.github.com>
2023-12-22 00:37:18 +0530

    Designing Sidebar and Links in Navbar

--- src/components/sidebar/links/Links.jsx
+++ src/components/sidebar/links/Links.jsx
@@ -1,47 +1,40 @@
 import { motion } from "framer-motion";
 
-const variants = {
-  open: {
-    transition: {
-      staggerChildren: 0.1,
-    },
-  },
-  closed: {
-    transition: {
-      staggerChildren: 0.05,
-      staggerDirection: -1,
-    },
-  },
-};
-const itemVariants = {
-  open: {
-    y: 0,
-    opacity: 1,
-  },
-  closed: {
-    y: 50,
-    opacity: 0,
-  },
-};
-
-const Links = () => {
-  const items = ["Homepage", "Services", "Portfolio", "Contact", "About"];
-
+const ToggleButton = ({ setOpen }) => {
   return (
-    <motion.div className="links" variants={variants}>
-      {items.map((item) => (
-        <motion.a
-          href={`#${item}`}
-          key={item}
-          variants={itemVariants}
-          whileHover={{ scale: 1.1 }}
-          whileTap={{ scale: 0.95 }}
-        >
-          {item}
-        </motion.a>
-      ))}
-    </motion.div>
+    <button onClick={() => setOpen((prev) => !prev)}>
+      <svg width="23" height="23" viewBox="0 0 23 23">
+        <motion.path
+          strokeWidth="3"
+          stroke="black"
+          strokeLinecap="round"
+          variants={{
+            closed: { d: "M 2 2.5 L 20 2.5" },
+            open: { d: "M 3 16.5 L 17 2.5" },
+          }}
+        />
+        <motion.path
+          strokeWidth="3"
+          stroke="black"
+          strokeLinecap="round"
+          d="M 2 9.423 L 20 9.423"
+          variants={{
+            closed: { opacity: 1 },
+            open: { opacity: 0 },
+          }}
+        />
+        <motion.path
+          strokeWidth="3"
+          stroke="black"
+          strokeLinecap="round"
+          variants={{
+            closed: { d: "M 2 16.346 L 20 16.346" },
+            open: { d: "M 3 2.5 L 17 16.346" },
+          }}
+        />
+      </svg>
+    </button>
   );
 };
 
-export default Links;
+export default ToggleButton;
